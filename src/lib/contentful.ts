30cc674121e4fdbfbf0c43003f5f1b6e9f15621c
mmpotulo28 <mpotulom28@gmail.com>
2024-12-10 16:29:39 +0200

    Add Contentful integration and update project configuration
    - Introduced Contentful client for managing content entries.
    - Added a new page for creating content entries.
    - Implemented dynamic routing for pages based on Contentful data.
    - Updated Next.js configuration for optimized image handling and export output.
    - Added Prettier configuration for consistent code formatting.
    - Removed unused API route.
    - Added VSCode settings for spell checking.

---
+++ src/lib/contentful.ts
@@ -0,0 +1,15 @@
+// lib/contentful.ts
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID || "",
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+});
+
+client.createEntry = async (contentType: string, fields: any) => {
+  return client.getSpace().then((space) => {
+    return space.createEntry(contentType, { fields });
+  });
+};
+
+export default client;
---
+++ src/pages/[...slug].tsx
@@ -0,0 +1,69 @@
+// pages/[...slug].tsx
+import { GetStaticPaths, GetStaticProps } from "next";
+import client from "../lib/contentful";
+import Image from "next/image";
+
+interface PageProps {
+  page: {
+    fields: {
+      title: string;
+      url: string;
+      topNavImage: { fields: { file: { url: string } } };
+    };
+    contentTypeId: string;
+  };
+}
+
+const Page = ({ page }: PageProps) => {
+  if (!page) return <div>Page not found</div>;
+
+  return (
+    <div>
+      <h1>{page.fields.title}</h1>
+      <Image
+        src={page.fields.topNavImage.fields.file.url}
+        alt={page.fields.title}
+        width={500} // replace with actual width
+        height={300} // replace with actual height
+      />
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "CorePage" });
+  const paths = res.items.map((item) => ({
+    params: {
+      slug: item.fields.url?.toString()?.split("/").filter(Boolean),
+    },
+  }));
+
+  console.log(
+    "paths: ",
+    paths.map((path) => path.params.slug?.join("/"))
+  );
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as { slug: string[] };
+  const res = await client.getEntries({
+    content_type: "CorePage",
+    "fields.url": `/${slug.join("/")}`,
+  });
+
+  console.log("res: ", res.items[0].fields);
+
+  if (!res.items.length) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      page: res.items[0],
+    },
+  };
+};
+
+export default Page;
